d2057612ce8c8717d3725b028a35b816c8dfffd4
ki7348 <ki7348@naver.com>
2024-04-07 21:28:41 +0900

    feature: SNS 아이콘 추가, 연결한 계정 닉네임 UI 구현

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Avatar from "@/components/Avatar/Avatar";
 import Colors from "@/styles/Color";
-import { StyleSheet, View } from "react-native";
+import Icon from "@/components/icon/Common";
 
 const Mypage = () => {
     return (
@@ -8,11 +10,20 @@ const Mypage = () => {
             <View style={styles.avatarContainer}>
                 <Avatar />
             </View>
+            <View style={styles.accountContainer}>
+                <Text style={styles.accountText}>연결한 계정</Text>
+                <View style={styles.emailContainer}>
+                    <Icon type="SNSKakaoM" />
+                    <Text style={styles.emailText}>abcdef@gmail.com</Text>
+                </View>
+            </View>
+            <View style={styles.nicknameContainer}>
+                <Text style={styles.nicknameText}>닉네임</Text>
+                <Text style={styles.nicknameCount}>0/10</Text>
+            </View>
         </View>
     );
 };
-
-export default Mypage;
 
 const styles = StyleSheet.create({
     container: {
@@ -23,4 +34,29 @@ const styles = StyleSheet.create({
     avatarContainer: {
         marginTop: 40,
     },
+    accountContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 72,
+        width: "90%",
+    },
+    accountText: {},
+    emailContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    emailText: {
+        marginLeft: 2,
+    },
+    nicknameContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 32,
+        width: "90%",
+    },
+    nicknameText: {},
+    nicknameCount: {},
 });
+
+export default Mypage;

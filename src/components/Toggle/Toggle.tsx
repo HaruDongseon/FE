aafa43cbb2179dd6bf4fd005488168f6b045e9af
ki7348 <ki7348@naver.com>
2024-05-01 16:49:54 +0900

    fetaure: 만들기 페이지에 toggle 2개 배치

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,5 +1,46 @@
-const Toggle = () => {
-    return <></>;
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "../icon/Common";
+import Colors from "@/styles/Color";
+
+interface ToggleComponentProps {
+    title: string;
+    expanded: boolean;
+    handlePress: () => void;
+}
+
+const Toggle: React.FC<ToggleComponentProps> = ({
+    title,
+    expanded,
+    handlePress,
+}) => {
+    return (
+        <TouchableOpacity style={styles.container} onPress={handlePress}>
+            <Text style={styles.title}>{title}</Text>
+            <Icon type={expanded ? "Up1LineR" : "Down1LineR"} />
+        </TouchableOpacity>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: 52,
+        paddingHorizontal: 20,
+        paddingVertical: 14,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: Colors.white,
+        borderBottomWidth: 1,
+        borderBottomColor: "#F0F2F2",
+        opacity: 1,
+    },
+    title: {
+        fontSize: 16,
+        color: Colors.grayScale600,
+        fontWeight: "700",
+    },
+});
 
 export default Toggle;
--- src/pages/Makingpage.tsx
+++ src/pages/Makingpage.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import Toggle from "@/components/Toggle/Toggle";
 import { RouteProp, useRoute } from "@react-navigation/native";
-import { Text } from "react-native";
 
 export type MypageParams = {
     Makingpage: {
@@ -8,14 +10,39 @@ export type MypageParams = {
 };
 
 const Makingpage: React.FC = () => {
+    const [firstToggleOpen, setFirstToggleOpen] = useState(true);
+    const [secondToggleOpen, setSecondToggleOpen] = useState(false);
     const route = useRoute<RouteProp<MypageParams, "Makingpage">>();
-    const date = route.params?.date;
 
     return (
-        <>
-            <Text>{date}</Text>
-        </>
+        <View style={styles.container}>
+            <Toggle
+                title="동선 기본 정보"
+                expanded={firstToggleOpen}
+                handlePress={() => {
+                    setFirstToggleOpen((prev) => !prev);
+                }}
+            />
+            <View style={styles.gap} />
+            <Toggle
+                title="동선 만들기"
+                expanded={secondToggleOpen}
+                handlePress={() => {
+                    setSecondToggleOpen((prev) => !prev);
+                }}
+            />
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+    },
+    gap: {
+        height: 8,
+    },
+});
 
 export default Makingpage;

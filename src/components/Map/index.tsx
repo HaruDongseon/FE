6f27610863f83fd04e00a78232bf26b499e90299
ki7348 <ki7348@naver.com>
2024-05-18 15:04:17 +0900

    feature: 안드로이드에서 Map 컴포넌트 렌더링 할 수 있도록 네이티브 코드 수정

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,14 +2,14 @@ import NaverMapView from "react-native-nmap";
 
 const Map = () => {
     const P0 = { latitude: 37.564362, longitude: 126.977011 };
-    const P1 = { latitude: 37.565051, longitude: 126.978567 };
-    const P2 = { latitude: 37.565383, longitude: 126.976292 };
 
     return (
         <NaverMapView
             style={{ width: "100%", height: "100%" }}
             showsMyLocationButton={true}
             center={{ ...P0, zoom: 16 }}
+            scaleBar={false}
+            zoomControl={false}
         ></NaverMapView>
     );
 };
